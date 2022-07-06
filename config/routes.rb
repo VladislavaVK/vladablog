a186e186c1f3c539769fe765979f61458dd7e002
mac-mini-102 <mac-mini-102@mac-mini-102s-Mac-mini.local>
2022-07-06 13:18:03 +0300

    Add authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'home/index'
+  devise_for :users
+  # get 'persons/profile'
+  root to: 'home#index'
+  # get 'persons/profile', as: 'user_root'
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+
+
+  end
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
