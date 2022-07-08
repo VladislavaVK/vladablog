86657f83dfaaffb696867aad910c331c41f49cc5
mac-mini-102 <mac-mini-102@mac-mini-102s-Mac-mini.local>
2022-07-08 17:27:52 +0300

    Ability to create and publish articles-итог

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,18 @@
 class Article < ApplicationRecord
-  include Visible
+  belongs_to :user
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 1 }
-end
+
+  VALID_STATUSES = ['public', 'draft']
+
+    validates :status, inclusion: { in: VALID_STATUSES }
+
+  def draft?
+      status == 'draft'
+  end
+
+    def public_count
+      self.where(status: 'public').count
+    end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
- get "/draft", to: "draft#index"
+ get 'draft/index'
   # get 'home/index'
   devise_for :users
   # get 'persons/profile'
-  root to: 'home#index'
-  # get 'persons/profile', as: 'user_root'
-  devise_scope :user do
-    get 'users/sign_out' => 'devise/sessions#destroy'
+  root 'home#index'
 
-   resources :articles
+  resources :articles
+
+  # get 'persons/profile', as: 'user_root'
+    get 'draft', to: "draft#index"
+
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'  
   end
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
