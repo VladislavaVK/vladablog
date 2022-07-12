8364d387035c00ae129cb1ffe45d91f5d8e465b0
mac-mini-102 <mac-mini-102@mac-mini-102s-Mac-mini.local>
2022-07-12 12:48:33 +0300

    исправлено

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,19 +1,19 @@
-module Visible
-  extend ActiveSupport::Concern
+#module Visible
+  #extend ActiveSupport::Concern
 
-  VALID_STATUSES = ['public', 'draft']
+  #VALID_STATUSES = ['public', 'draft']
 
-  included do
-    validates :status, inclusion: { in: VALID_STATUSES }
-  end
+  #included do
+    #validates :status, inclusion: { in: VALID_STATUSES }
+  #end
 
-  class_methods do
-    def public_count
-      where(status: 'public').count
-    end
-  end
+  #class_methods do
+    #def public_count
+     # where(status: 'public').count
+    #end
+  #end
 
-  def draft?
-    status == 'draft'
-  end
-end
+ # def draft?
+ #   status == 'draft'
+ # end
+#end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
  get 'draft/index'
-  # get 'home/index'
   devise_for :users
-  # get 'persons/profile'
   root 'home#index'
 
   resources :articles
 
-  # get 'persons/profile', as: 'user_root'
     get 'draft', to: "draft#index"
 
     devise_scope :user do
       get '/users/sign_out' => 'devise/sessions#destroy'  
   end
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
